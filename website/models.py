7cc8549c7026546b47ed90a0891347ef7decf528
not-pro <not-pro@example.com>
2023-09-17 15:33:21 -0700

    Added model Review.

--- website/models.py
+++ website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Review(models.Model):
+    name = models.TextField(max_length=500)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        """Return string"""
+        return self.name
